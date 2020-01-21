34d41e0a9cf4d59d07e255ea3d41ed85b664de76
Peder Landsverk <pglandsverk@gmail.com>
2020-01-21 14:02:16 +0100

    Cleanup
    Now removes files after 5 minutes.
    This means that huge gifs might be removed from server before the user
    has "re-downloaded" them.
    
    This change made me think about what a weird app this is.

--- infogif.go
+++ infogif.go
@@ -10,12 +10,21 @@ import (
 	"encoding/hex"
 	"os"
 	"path"
+	"time"
 	)
 
 
 type Gif struct {
 	Title string
 	Path string
+}
+
+func fileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
 }
 
 func baseHandler(writer http.ResponseWriter, request *http.Request) {
@@ -51,62 +60,67 @@ func uploadHandler(writer http.ResponseWriter, request *http.Request){
 	}
 	*/
 
-	srcFile, _, err := request.FormFile("file")
+	uploadedFile, _, err := request.FormFile("file")
 	if err != nil {
 		log.Print("Failed to acquire the uploaded content")
 		writer.WriteHeader(http.StatusInternalServerError)
 		//writeError(writer, err)
 		return
 	}
-
-	defer srcFile.Close()
-	//log.Print(info)
-
-	/*
-	size,err := getSize(srcFile)
-	if err != nil {
-		log.Print("Failed to get size of file") 
-		writer.Writeheader(http.StatusInternalServerError)
-		//writeError(writer,err)
-		return
-	}
-	*/
-
-	body, err := ioutil.ReadAll(srcFile)
+	defer uploadedFile.Close()
+	// Read content
+	body, err := ioutil.ReadAll(uploadedFile)
 	if err != nil {
 		log.Print("Failed to read the uploaded content") 
 		writer.WriteHeader(http.StatusInternalServerError)
 		//writeError(writer,err)
 		return
 	}
+
+	// Gif content is hashed to assert identity.
 	filehash := md5.Sum(body)
 	filehashDigest := hex.EncodeToString(filehash[:])
-	/*
-	Make it return if it already has that gif, reducing disk writes.
-	*/
 
+	// Check if file already exists
 	dstPath := path.Join("static/gifs", filehashDigest)
-	dstFile, err := os.OpenFile(dstPath, os.O_WRONLY | os.O_CREATE | os.O_TRUNC, 0666)
-	if err != nil {
-		log.Print("Failed to open file!")
-		writer.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	defer dstFile.Close()
-
-	_ , err = dstFile.Write(body); if err != nil {
-		log.Print("Failed to write content!")
-		writer.WriteHeader(http.StatusInternalServerError)
-		return
+	if !fileExists(dstPath){
+		// Write the data to a new file 
+		dstFile, err := os.OpenFile(dstPath, os.O_WRONLY | os.O_CREATE | os.O_TRUNC, 0666)
+		if err != nil {
+			log.Print("Failed to open file!")
+			writer.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		defer dstFile.Close()
+		_ , err = dstFile.Write(body); if err != nil {
+			log.Print("Failed to write content!")
+			writer.WriteHeader(http.StatusInternalServerError)
+			return
+		} 
+		defer cleanup(dstPath)
 	} else {
-		fmt.Fprintf(writer, filehashDigest)
+		log.Print(fmt.Sprintf("%s already exists",dstFile))
 	}
+	fmt.Fprintf(writer, filehashDigest)
 }
+
+
+func cleanup(path string){
+	deleteFile:= func(){
+		log.Print(fmt.Sprintf("Deleting %s",path))
+		err := os.Remove(path)
+		if err != nil {
+			log.Print(err)
+		}
+	}
+	// Deletes a file after X minutes, since this is not a file hosting service.
+	time.AfterFunc(5*time.Minute, deleteFile) 
+}
+
 
 func main() {
 	http.HandleFunc("/",baseHandler)
 	http.HandleFunc("/upload/",uploadHandler)
-
 	http.Handle("/static/", http.FileServer(http.Dir(".")))
 	log.Fatal(http.ListenAndServe(":9090",nil))
 }
